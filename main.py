d043df7060ae426b18baf712b43e453ccdf5e475
Yuguang <yuguanglee2003@gmail.com>
2025-10-28 14:51:55 -0400

    yuh

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def main():
     newgame.display()
 
 def main_2():
+    newgame = None
     ip = "0.0.0.0" #defualt
     port = 5432#default
     if(len(sys.argv) == 4):
@@ -18,6 +19,6 @@ def main_2():
         print("WRONG NUMBER OF ARGUMENTS")
         print("usage: p2p.py <host/connect> <ip> <port>")
         print("ex: p2p.py host 0.0.0.0  2020")
-    print("done")
+    newgame.close_all()
 #main()
 main_2()
